716a50a61a7e7a5c43bed1ab0819565227584344
Tim de Jong <49719019+jongdetim@users.noreply.github.com>
2022-06-12 15:34:05 +0000

    added histogram.py

---
+++ histogram.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%%
+data = pd.read_csv('datasets/dataset_train.csv')
+colors = iter(['red', '#b5651d', 'blue', 'green'])
+grps = data.groupby(['Hogwarts House'])
+#%%
+for key, grp in grps:
+    plt.hist(grp['Care of Magical Creatures'], label=key, alpha=0.5, color=next(colors))
+plt.legend()
+plt.title('Care of Magical Creatures')
+plt.ylabel('Number of Students')
+plt.xlabel('Grade')
+plt.show()
+# %%
