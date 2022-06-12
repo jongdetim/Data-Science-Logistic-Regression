4edda25def71dfe3fcc1073265334a2cf5d1b279
Tim de Jong <49719019+jongdetim@users.noreply.github.com>
2022-06-12 16:00:10 +0000

    added scatter_plot.py

---
+++ scatter_plot.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('datasets/dataset_train.csv')
+x, y = 'Defense Against the Dark Arts', 'Astronomy'
+plt.scatter(data[x], data[y], s=15)
+plt.xlabel(x)
+plt.ylabel(y)
+plt.show()
