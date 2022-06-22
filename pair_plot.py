2e9147899b1486658c7339088a214feb36cc2572
Tim de Jong <49719019+jongdetim@users.noreply.github.com>
2022-06-22 13:45:54 +0000

    added house color coding for pairplot

--- pair_plot.py
+++ pair_plot.py
@@ -5,12 +5,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #%% read
-data = pd.read_csv('datasets/dataset_train.csv')
+orig_data = pd.read_csv('datasets/dataset_train.csv')
 
 #%% clean
-data = data.select_dtypes([np.number])
+data = orig_data.select_dtypes([np.number])
 data = data.drop(['Index','index'], axis=1, errors='ignore')
+data.insert(0, 'Hogwarts House', orig_data['Hogwarts House'])
+data = data[data[:, 0].argsort()]
 
 #%% plot
-sns.pairplot(data)
+palette = {'Gryffindor' : 'red', 'Ravenclaw' : 'light blue', 'Slytherin' : 'green', 'Hufflepuff' : '#b5651d'}
+sns.pairplot(data, diag_kind='hist', hue='Hogwarts House', palette=palette, plot_kws={'alpha':0.4})
 plt.show()
+
+# %%
